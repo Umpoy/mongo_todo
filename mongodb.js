25984c21ab31702bad391c2234c30e11857d57bc
Ian <iumpoy@yahoo.com>
2020-01-04 21:52:33 -0800

    added first collection

--- mongodb.js
+++ mongodb.js
@@ -2,11 +2,17 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const database = 'todo-manager';
+const databaseName = 'todo-manager';
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database');
     }
-    console.log('Connected to database');
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'admin',
+        age: '27'
+    });
 })
