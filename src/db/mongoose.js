93e4de92ce062225bdb64e2ee6ba6001bffede14
Ian <iumpoy@yahoo.com>
2020-01-07 18:51:08 -0800

    added npm validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -14,13 +14,34 @@ const User = mongoose.model('User', {
     }
 });
 
-const me = new User({
-    name: 'Ian',
-    age: 27
-});
-
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error: ', error)
+const Todo = mongoose.model('Todo', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
+
+const task = new Todo({
+    description: 'code out schema',
+    completed: true
+})
+
+task.save().then(() => {
+    console.log(task);
+}).catch(error => {
+    console.log(error);
+})
+
+// const me = new User({
+//     name: 'Ian',
+//     age: 27
+// });
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error: ', error)
+// })
+
