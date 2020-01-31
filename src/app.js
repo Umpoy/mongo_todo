fa2a00fd48813421554a25ee601e81c357269000
Ian <iumpoy@yahoo.com>
2020-01-31 15:45:25 -0800

    added status codes onto endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,11 +12,21 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
-    }).catch((event) => {
-        res.send(event)
+        res.status(201).send(user);
+    }).catch(event => {
+        res.status(400).send(event);
     });
 });
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch(event => {
+        res.status(400).send(event);
+    })
+})
 
 app.listen(port, () => {
     console.log("Server running on port: " + port);
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,14 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/todo-manager-api', {
     useCreateIndex: true
 });
 
-const Todo = mongoose.model('Todo', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
