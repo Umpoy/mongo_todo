8268973a6688b8200201177849ca13095b99709c
Ian <iumpoy@yahoo.com>
2020-02-01 23:11:35 -0800

    added gitignore

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/todo-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    userFindAndModify: false
 });
 
