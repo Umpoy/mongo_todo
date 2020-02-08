0c5534e2c5f9de31640e92e79240b03908a3adee
Ian <iumpoy@yahoo.com>
2020-02-07 22:59:14 -0800

    revised update endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/todo-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    userFindAndModify: false
+    userFindAndModify: false,
+    useUnifiedTopology: true
 });
 
